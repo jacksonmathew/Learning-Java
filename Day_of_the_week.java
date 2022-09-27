e85090c2a1907de46bc6b213f0422f1c8fdd7832
CHACKOCHAN <75202241+jacksonmathew@users.noreply.github.com>
2022-09-27 05:53:10 +0530

    Create Day_of_the_week.java

---
+++ Day_of_the_week.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class daty_prgm {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        switch (num){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+        }
+    }
+}
